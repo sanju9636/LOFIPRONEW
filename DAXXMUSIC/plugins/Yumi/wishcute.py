0c501d2ff1f178df8bcaf72cd63867d0d44a48af
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 13:39:39 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -19,7 +19,7 @@ async def wish(_, m):
     wish = f"๏ ʜᴇʏ {m.from_user.first_name}!"
     wish += f"\n๏ ʏᴏᴜʀ ᴡɪꜱʜ ➠ {text} "
     wish += f"\n๏ ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ ➠ {wish_count}%"
-    wish += f"\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐"
+    wish += f"\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼"
     
     await app.send_animation(
         chat_id=m.chat.id,
