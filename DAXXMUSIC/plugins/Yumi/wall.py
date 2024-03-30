5ac91d3255e1f3c90035e3ca3d8ef92785ff29f4
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 13:36:30 +0530

    Update wall.py

--- DAXXMUSIC/plugins/Yumi/wall.py
+++ DAXXMUSIC/plugins/Yumi/wall.py
@@ -44,7 +44,7 @@ def pic_command(client, message: Message):
     try:
         response = requests.get(unsplash_url)
         if response.status_code == 200:
-            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"✦ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➠ {name}.\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐" )
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"✦ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➠ {name}.\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼" )
         else:
             client.send_message(message.chat.id, "✦ Failed to fetch image.")
     except requests.RequestException as e:
