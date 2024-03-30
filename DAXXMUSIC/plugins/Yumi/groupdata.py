30d5dde7c70bf1d7ff51df97947378e3e88e2adf
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 13:03:09 +0530

    Update groupdata.py

--- DAXXMUSIC/plugins/Yumi/groupdata.py
+++ DAXXMUSIC/plugins/Yumi/groupdata.py
@@ -46,7 +46,7 @@ async def instatus(app, message):
 ๏ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs ➠ {premium_acc} 
 ๏ ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➠ {timelog} 
 
-๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐ **""")
+๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼ **""")
     else:
         sent_message = await message.reply_text("✦ ONLY ADMINS CAN USE THIS !")
         await sleep(5)
