a03f98f497ca09a876c7636e3c38fb7407e84b5d
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 15:41:49 +0530

    Update gitinfo.py

--- DAXXMUSIC/plugins/tools/gitinfo.py
+++ DAXXMUSIC/plugins/tools/gitinfo.py
@@ -19,7 +19,7 @@ async def blackpink(_, message):
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git NYKAAXBOT")
+        await message.reply_text("/git OWNER SE PUCH")
         return
 
     username = message.text.split(None, 1)[1]
