7246cdc9d0b392f25356ea9de1c4b31b4d1173b0
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 14:25:42 +0530

    Update ai.py

--- DAXXMUSIC/plugins/tools/ai.py
+++ DAXXMUSIC/plugins/tools/ai.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                         f"♥︎ {x}  \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  ๛ɴ ʏ ᴋ ᴀ ᴀ࿐",
+                         f"♥︎ {x}  \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
