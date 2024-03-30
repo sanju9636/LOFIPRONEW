1c451eed0e058f18e1dc93615a3fe0ab1e03900a
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-30 13:35:41 +0530

    Update video.py

--- DAXXMUSIC/plugins/Yumi/video.py
+++ DAXXMUSIC/plugins/Yumi/video.py
@@ -83,7 +83,7 @@ async def ytmusic(client, message: Message):
         return
     c_time = time.time()
     file_stark = f"{ytdl_data['id']}.mp4"
-    capy = f"✦ **ᴛɪᴛʟᴇ ➠** [{thum}]({mo})\n\n๏ **ᴄʜᴀɴɴᴇʟ ➠** {thums}\n๏ **sᴇᴀʀᴄʜᴇᴅ ➠** {urlissed}\n๏ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠** {chutiya}\n\n**๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐**"
+    capy = f"✦ **ᴛɪᴛʟᴇ ➠** [{thum}]({mo})\n\n๏ **ᴄʜᴀɴɴᴇʟ ➠** {thums}\n๏ **sᴇᴀʀᴄʜᴇᴅ ➠** {urlissed}\n๏ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠** {chutiya}\n\n**๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼**"
     await client.send_video(
         message.chat.id,
         video=open(file_stark, "rb"),
